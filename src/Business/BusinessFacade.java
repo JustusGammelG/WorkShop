f154b159de8012c6b791e16cb0586d9ba8e475d7
Alexa <Alexa@DESKTOP-KFM6FS8.home>
2017-11-22 17:59:53 +0100

    BusinessLayer -> Business
    -Rol

---
+++ src/Business/BusinessFacade.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import Acquaintance.IBuilding;
+import Acquaintance.IReading;
+import Acquaintance.ISensor;
+
+/**
+ *
+ * @author Alexa
+ */
+public class BusinessFacade implements  {
+    
+}
---
+++ src/Business/Reading.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+import Acquaintance.IReading; 
+import java.util.Date;
+
+/**
+ *
+ * @author sdown
+ */
+public class Reading implements IReading{
+    double value;
+    Date date;
+    String id;
+
+    public Reading(double value) {
+        this.date = new Date();
+        this.value = value;
+       
+    }
+    
+    
+    public String toString(){
+        return "Timestamp: " + date + "\n Value: " + value;
+    }
+}
