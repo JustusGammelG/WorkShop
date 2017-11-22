ac0de7a4cec5aa76e9a0e406737787bc45dd1778
Justus <Justus@JustusLenovo>
2017-11-22 11:04:48 +0100

    Created some interfaces and stuff

---
+++ src/workshopoop/IBuilding.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshopoop;
+
+/**
+ *
+ * @author Justus
+ */
+public interface IBuilding {
+    ArrayList<> sensors = new ArrayList<Sensor>;
+    String adress;
+    
+}
