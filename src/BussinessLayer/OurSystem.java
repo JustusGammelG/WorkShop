06e67bebc93e05efce5136d5b74e498bb9c1a604
Justus <Justus@JustusLenovo>
2017-11-22 13:20:19 +0100

    EVERYTHING

---
+++ src/BussinessLayer/OurSystem.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BussinessLayer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Justus
+ */
+public class OurSystem {
+    HashMap<String, Building> buildings = new HashMap<String, Building>();
+    public void addBuilding(String name, Building building){
+        buildings.put(name, building);
+    }
+    
+    public void removeBuilding(String name){
+        buildings.remove(name);
+    }
+    
+    public void printBuildings(){
+        for(Map.Entry<String, Building> entry: buildings.entrySet()){
+            System.out.println("Name: " + entry.getKey() + entry.getValue());
+        }
+    }
+    
+    public void printBuildingReadings(Building building){
+        building.printBuildingReadings();
+    }
+ 
+    public static void main(String[] args) {
+        Building building1 = new Building("Building1", "Posthuset");
+        Building building2 = new Building("Building2", "Posthuset123");
+        Building building3 = new Building("Building3", "Posthuset321");
+        
+        OurSystem system = new OurSystem();
+        
+        
+        system.addBuilding("Building1", building1);
+        system.addBuilding("Building2", building2);
+        system.addBuilding("Building3", building3);
+        
+        system.printBuildings();
+        System.out.println(" removing.....");
+        system.removeBuilding("Building1");
+        system.printBuildings();
+        
+        Sensor sensor1 = new Sensor("TempSensor1");
+        building3.addSensor(sensor1);
+        sensor1.setReading(50);
+        sensor1.setReading(100);
+        sensor1.setReading(200);
+        
+        system.printBuildingReadings(building3);
+        
+    }
+}
